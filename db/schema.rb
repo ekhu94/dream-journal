cabcfe03ea103162302c54ba39b0d8d99ee65d34
Erik Huang <erikhuangdrums@gmail.com>
2021-01-26 06:29:22 -0800

    Revert "create entries table, reset seeds"
    This reverts commit 996208017bc31270c713c4dda12dd104f08b21ae.

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_134724) do
+ActiveRecord::Schema.define(version: 2021_01_26_012249) do
 
   create_table "dreams", force: :cascade do |t|
     t.string "category"
     t.integer "remembrance"
     t.boolean "recurring"
     t.integer "person_id"
-  end
-
-  create_table "entries", force: :cascade do |t|
-    t.datetime "date"
-    t.text "subject"
-    t.integer "hours_slept"
-    t.boolean "recurring"
-    t.integer "dream_id"
   end
 
   create_table "people", force: :cascade do |t|
