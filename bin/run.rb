086e71b399b09bcd36a9fbe3d88ff84387f52726
Erik Huang <erikhuangdrums@gmail.com>
2021-01-26 10:57:53 -0800

    revert to previous commit, restored entry table, comment out cli.rb

--- bin/run.rb
+++ bin/run.rb
@@ -1,4 +1,5 @@
 require_relative '../config/environment'
-require_relative '../lb/cli.rb'
+# require_relative '../lb/cli.rb'
 
 puts "HELP!"
+
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,15 +1,15 @@
-require_relative '../config/environment'
-require_all '../app/models'
+# require_relative '../config/environment'
+# require_all '../app/models'
 
-class CLI
-    def welcome
-        puts "Welcome to your Dream Journal."
-    end
+# class CLI
+#     def welcome
+#         puts "Welcome to your Dream Journal."
+#     end
 
-    def log_in
-        welcome
-        puts "Please enter your name:"
-        name = gets.chomp
-        "Hello #{name}"
-    end
-end
+#     def log_in
+#         welcome
+#         puts "Please enter your name:"
+#         name = gets.chomp
+#         "Hello #{name}"
+#     end
+# end
