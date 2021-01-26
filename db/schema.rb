390855532ae184f1308b35068d55157555a47fe9
Erik Huang <erikhuangdrums@gmail.com>
2021-01-25 17:29:35 -0800

    create migration for dreams

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_25_215033) do
+ActiveRecord::Schema.define(version: 2021_01_26_012249) do
+
+  create_table "dreams", force: :cascade do |t|
+    t.string "category"
+    t.integer "remembrance"
+    t.boolean "recurring"
+    t.integer "person_id"
+  end
 
   create_table "people", force: :cascade do |t|
     t.string "name"
